5643b1ad3dce01366d1d754af4200bce2a8666b7
sumith prabhu <sumithprabhu07@gmail.com>
2023-02-04 00:18:10 +0530

    Code added

---
+++ Basic Data Types.cpp
@@ -0,0 +1,21 @@
+//https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    scanf("%d %ld %c %f %lf",&a,&b,&c,&d,&e);
+    printf("%d \n",a);
+    printf("%ld \n",b);
+    printf("%c \n",c);
+    printf("%f \n",d);
+    printf("%lf \n",e);
+    return 0;
+}
